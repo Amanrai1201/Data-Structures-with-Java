6bcad0b269717ae2ae010f1fad51c4dee1e2699a
Amanrai1201 <amanrai12012005@gmail.com>
2026-03-03 14:23:04 +0530

    Add LinkedHashmap implementation with example usage

---
+++ Hashmap/LinkedHashmap.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashMap;
+
+public class LinkedHashmap {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> LHM = new LinkedHashMap<>();
+
+        LHM.put("India", 100);
+        LHM.put("China", 150);
+        LHM.put("Russia", 80);
+
+        System.out.println(LHM);
+        // ye sequencial hoti hai na ki random
+
+    }
+}
