b0a22574466f952dfe990f1c992c3220ba5e0874
Amanrai1201 <amanrai12012005@gmail.com>
2026-03-03 14:17:30 +0530

    implementation of hashmap

--- Hashmap/practice.java
+++ Hashmap/practice.java
@@ -6,14 +6,20 @@ public class practice {
         HashMap<String, Integer> hm = new HashMap<>();
 
         // insert
-        hm.put("Aman", 011);
+        hm.put("Aman", 11);
         hm.put("Mehak", 61);
         hm.put("Himanshu", 44);
         hm.put("Mahak", 59);
 
         // System.err.println(hm);
 
-        System.out.println(hm.get("Himanshu"));
+        // System.out.println(hm.get("Himanshu"));
+        Set<String> keys = hm.keySet();
+        // System.err.println(keys);
+
+        for (String val : keys) {
+            System.out.println("key " + val + " values " + hm.get(val));
+        }
 
     }
 }
